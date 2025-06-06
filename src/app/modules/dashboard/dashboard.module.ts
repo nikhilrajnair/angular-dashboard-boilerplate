afe93e6f18909bf4d76a5f86af0d424c26a7613b
Nikhil Raj A <nikhilrajnair.a@gmail.com>
2025-06-06 20:00:26 +0200

    feat: updated dashboard with charts and get users via data fetching

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [SharedModule, DashboardRoutingModule, RouterModule],
+  imports: [SharedModule, DashboardRoutingModule, RouterModule, NgChartsModule],
 })
 export class DashboardModule {}
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -5,9 +5,11 @@ import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserComponent } from './user.component';
-
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   declarations: [UserComponent],
-  imports: [SharedModule, UserRoutingModule],
+  imports: [SharedModule, UserRoutingModule, FormsModule, MatFormFieldModule, MatInputModule],
 })
 export class UserModule {}
